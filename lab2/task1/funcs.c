f7cf36ebca515a3819dd252232bb17edf335e6dd
TimeB <tamerlanirisov080@gmail.com>
2023-10-24 23:20:05 +0300

    -

--- lab2/task1/funcs.c
+++ lab2/task1/funcs.c
@@ -3,6 +3,18 @@
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
+#include<stdbool.h>
+
+
+
+
+bool is_num(int num){
+    if ((num > 47) && (num < 58)){
+        return true;
+    }
+    return false;
+}
+
 
 enum flags command_rec(int argc,char** argv) {
 	char* string = argv[1];
@@ -35,6 +47,7 @@ enum flags command_rec(int argc,char** argv) {
     
 }
 
+
 void reverse(char * string,int* status){
     long int string_len = strlen(string);
     char* answer = malloc(string_len*sizeof(char));
@@ -64,13 +77,34 @@ void odd_up_str(char* string,int* status){
 
 void numb_symb_str(char* string, int* status){
     long int string_len = strlen(string);
-    char* answer = malloc(string_len*sizeof(char));
-    if (answer == NULL){
+    char* symbs = malloc(sizeof(char)*string_len);
+    if (symbs == NULL){
         *status = 1;
         return;
     }
-    for (int i = string_len - 1; i > -1; i-- ){
-        answer[string_len-i-1] = string[i];
+    int index_counter_nums = 0;
+    int index_counter_symbs = 0;
+    for (int i = 0; i < string_len; i++ ){
+        if (is_num(string[i])){
+            string[index_counter_nums] = string[i];
+            index_counter_nums++;
+        }
+        else{
+            symbs[index_counter_symbs] = string[i];
+            index_counter_symbs++;
+        }
     }
+    for (int i = 0; i < index_counter_symbs; i++){
+        string[index_counter_nums] = symbs[i];
+        index_counter_nums++;
+    }  
     
+
+    return;
+    
+}
+void concatenation_str(char** argv,int* status){
+    int string_quantity = strtol(argv[2]);
+
+
 }
--- lab2/task1/lab2task1.c
+++ lab2/task1/lab2task1.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv){
     //say hello
-    int* status = 0;
+    int* status = malloc(sizeof(int));
     char* string = argv[2];
     long int string_len = strlen(argv[2]);
     switch (command_rec(argc,argv))
@@ -19,16 +19,30 @@ int main(int argc, char** argv){
             break;
         case reverse_str:
             reverse(string,status);
+            if (*status == 1){
+                printf("Memore allocation error!\n");
+                break;
+            }
             printf("%s\n",string);
             break;
         case odd_up:
             odd_up_str(string,status);
+            if (*status == 1){
+                printf("Memore allocation error!\n");
+                break;
+            }
             printf("%s \n",string);
             break;
         case num_symb:
-            
+            numb_symb_str(string,status);
+            if (*status == 1){
+                printf("Memore allocation error!\n");
+                break;
+            }
+            printf("%s \n",string);
             break;
-
+        case concatenation:
+            
    
 
 
