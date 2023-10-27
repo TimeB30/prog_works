0042156f95048fc5c4ab1d847dfaf4ef94ed851d
TimeB <tamerlanirisov080@gmail.com>
2023-10-27 19:37:11 +0300

    -

---
+++ lab2/task3/lab2task3.c
@@ -0,0 +1,11 @@
+#include"funcs.h"
+
+int main(){
+    int status = 0;
+    
+
+
+
+
+    return 0;
+}
--- lab2/task7/lab2task7.c
+++ lab2/task7/lab2task7.c
@@ -7,28 +7,7 @@ double equation(int x){
 
 }
 
-double find_root(int left_border, int right_border, double eps,double (*equation_ptr)(int),double* status){
-    double result  = equation_ptr(3);
-    double answer = 0;
-    if ((left_border > result) || (right_border < result)){
-        *status = 1;
-        return answer;
-    }
-    else if (eps < 0){
-        *status = 2;
-        return answer;
-    }
-    while(abs(result - answer) > eps){       
-        answer =  (right_border+left_border)/2;
-        if (answer > result){
-            right_border = answer;
-        }
-        else{
-            left_border = answer;
-        }
-         printf("result %lf  answer   %lf\n",result, answer);
-    }
-}
+
 
 int main(){
     double status = 0;
